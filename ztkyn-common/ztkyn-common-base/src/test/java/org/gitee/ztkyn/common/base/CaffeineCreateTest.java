a0665eaec120f431342805bdced1d486a97fda63
ZtKyn <ZtKyn@lazier.work>
2023-01-30 17:16:47 +0800

    2023-01-30

---
+++ ztkyn-common/ztkyn-common-base/src/test/java/org/gitee/ztkyn/common/base/CaffeineCreateTest.java
@@ -0,0 +1,167 @@
+package org.gitee.ztkyn.common.base;
+
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 总结
+ * 上述一些策略在创建时都可以进行自由组合，一般情况下有两种方法
+ * 设置 maxSize、refreshAfterWrite，不设置 expireAfterWrite/expireAfterAccess
+ * 设置expireAfterWrite当缓存过期时会同步加锁获取缓存，所以设置expireAfterWrite时性能较好，但是某些时候会取旧数据,适合允许取到旧数据的场景
+ * 设置 maxSize、expireAfterWrite/expireAfterAccess，不设置 refreshAfterWrite
+ * 数据一致性好，不会获取到旧数据，但是性能没那么好（对比起来），适合获取数据时不耗时的场景
+ *
+ * @author whty
+ * @version 1.0
+ */
+public class CaffeineCreateTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(CaffeineCreateTest.class);
+
+	/**
+	 * 手动创建
+	 * 最普通的一种缓存，无需指定加载方式，需要手动调用put()进行加载。需要注意的是put()方法对于已存在的key将进行覆盖，
+	 * 这点和Map的表现是一致的。在获取缓存值时，如果想要在缓存值不存在时，原子地将值写入缓存，则可以调用get(key,
+	 * k -> value)方法，该方法将避免写入竞争。调用invalidate()方法，将手动移除缓存。
+	 * 在多线程情况下，当使用get(key, k ->
+	 * value)时，如果有另一个线程同时调用本方法进行竞争，则后一线程会被阻塞，直到前一线程更新缓存完成；
+	 * 而若另一线程调用getIfPresent()方法，则会立即返回null，不会被阻塞。
+	 */
+	@Test
+	public void manualCreate() {
+		Cache<Object, Object> cache = Caffeine.newBuilder()
+				// 初始数量
+				.initialCapacity(10)
+				// 最大条数
+				.maximumSize(10)
+				// expireAfterWrite和expireAfterAccess同时存在时，以expireAfterWrite为准
+				// 最后一次写操作后经过指定时间过期
+				.expireAfterWrite(1, TimeUnit.SECONDS)
+				// 最后一次读或写操作后经过指定时间过期
+				.expireAfterAccess(1, TimeUnit.SECONDS)
+				// 监听缓存被移除
+				.removalListener((key, val, removalCause) -> {
+				})
+				// 记录命中
+				.recordStats().build();
+
+		cache.put("1", "张三");
+		// 张三
+		System.out.println(cache.getIfPresent("1"));
+		// 存储的是默认值
+		System.out.println(cache.get("2", o -> "默认值"));
+	}
+
+	/**
+	 * LoadingCache是一种自动加载的缓存。其和普通缓存不同的地方在于，当缓存不存在/缓存已过期时，若调用get()方法，
+	 * 则会自动调用CacheLoader.load()方法加载最新值。调用getAll()方法将遍历所有的key调用get()，
+	 * 除非实现了CacheLoader.loadAll()方法。使用LoadingCache时，需要指定CacheLoader，并实现其中的load()方法供缓存缺失时自动加载。
+	 * 在多线程情况下，当两个线程同时调用get()，则后一线程将被阻塞，直至前一线程更新缓存完成。
+	 */
+	@Test
+	public void loadingCacheTest() {
+		LoadingCache<String, String> loadingCache = Caffeine.newBuilder()
+				// 创建缓存或者最近一次更新缓存后经过指定时间间隔，刷新缓存；refreshAfterWrite仅支持LoadingCache
+				.refreshAfterWrite(10, TimeUnit.SECONDS).expireAfterWrite(10, TimeUnit.SECONDS)
+				.expireAfterAccess(10, TimeUnit.SECONDS).maximumSize(10)
+				// 根据key查询数据库里面的值，这里是个lamba表达式
+				.build(key -> new Date().toString());
+	}
+
+	/**
+	 * AsyncCache是Cache的一个变体，其响应结果均为CompletableFuture，通过这种方式，AsyncCache对异步编程模式进行了适配。
+	 * 默认情况下，缓存计算使用ForkJoinPool.commonPool()作为线程池，如果想要指定线程池，则可以覆盖并实现Caffeine.executor(Executor)方法。
+	 * synchronous()提供了阻塞直到异步缓存生成完毕的能力，它将以Cache进行返回。
+	 * 在多线程情况下，当两个线程同时调用get(key, k ->
+	 * value)，则会返回同一个CompletableFuture对象。由于返回结果本身不进行阻塞，可以根据业务设计自行选择阻塞等待或者非阻塞。
+	 */
+	@Test
+	public void asyncLoadingCacheTest() {
+		AsyncLoadingCache<String, String> asyncLoadingCache = Caffeine.newBuilder()
+				// 创建缓存或者最近一次更新缓存后经过指定时间间隔刷新缓存；仅支持LoadingCache
+				.refreshAfterWrite(1, TimeUnit.SECONDS).expireAfterWrite(1, TimeUnit.SECONDS)
+				.expireAfterAccess(1, TimeUnit.SECONDS).maximumSize(10)
+				// 根据key查询数据库里面的值
+				.buildAsync(key -> {
+					Thread.sleep(1000);
+					return new Date().toString();
+				});
+
+		// 异步缓存返回的是CompletableFuture
+		CompletableFuture<String> future = asyncLoadingCache.get("1");
+		future.thenAccept(System.out::println);
+	}
+
+	/**
+	 * refreshAfterWrite()表示x秒后自动刷新缓存的策略可以配合淘汰策略使用，注意的是刷新机制只支持LoadingCache和AsyncLoadingCache
+	 */
+	private static int NUM = 0;
+
+	@Test
+	public void refreshAfterWriteTest() throws InterruptedException {
+		LoadingCache<Integer, Integer> cache = Caffeine.newBuilder()
+				.refreshAfterWrite(1, TimeUnit.SECONDS)
+				//模拟获取数据，每次获取就自增1
+				.build(integer -> ++NUM);
+
+		//获取ID=1的值，由于缓存里还没有，所以会自动放入缓存
+		System.out.println(cache.get(1));// 1
+
+		// 延迟2秒后，理论上自动刷新缓存后取到的值是2
+		// 但其实不是，值还是1，因为refreshAfterWrite并不是设置了n秒后重新获取就会自动刷新
+		// 而是x秒后&&第二次调用getIfPresent的时候才会被动刷新
+		Thread.sleep(2000);
+		System.out.println(cache.getIfPresent(1));// 1
+
+		//此时才会刷新缓存，而第一次拿到的还是旧值
+		System.out.println(cache.getIfPresent(1));// 2
+	}
+
+	/**
+	 * 统计
+	 */
+	public void recordStatsTest() {
+		LoadingCache<String, String> cache = Caffeine.newBuilder()
+				//创建缓存或者最近一次更新缓存后经过指定时间间隔，刷新缓存；refreshAfterWrite仅支持LoadingCache
+				.refreshAfterWrite(1, TimeUnit.SECONDS)
+				.expireAfterWrite(1, TimeUnit.SECONDS)
+				.expireAfterAccess(1, TimeUnit.SECONDS)
+				.maximumSize(10)
+				//开启记录缓存命中率等信息
+				.recordStats()
+				//根据key查询数据库里面的值
+				.build(key -> {
+					Thread.sleep(1000);
+					return new Date().toString();
+				});
+
+
+		cache.put("1", "shawn");
+		cache.get("1");
+
+		/*
+		 * hitCount :命中的次数
+		 * missCount:未命中次数
+		 * requestCount:请求次数
+		 * hitRate:命中率
+		 * missRate:丢失率
+		 * loadSuccessCount:成功加载新值的次数
+		 * loadExceptionCount:失败加载新值的次数
+		 * totalLoadCount:总条数
+		 * loadExceptionRate:失败加载新值的比率
+		 * totalLoadTime:全部加载时间
+		 * evictionCount:丢失的条数
+		 */
+		System.out.println(cache.stats());
+	}
+
+}
---
+++ ztkyn-common/ztkyn-common-base/src/test/java/org/gitee/ztkyn/common/base/CaffeineExpireTest.java
@@ -0,0 +1,124 @@
+package org.gitee.ztkyn.common.base;
+
+import java.util.concurrent.TimeUnit;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Scheduler;
+import com.github.benmanes.caffeine.cache.Weigher;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 驱逐策略在创建缓存的时候进行指定。常用的有基于容量的驱逐和基于时间的驱逐。
+ *
+ * 基于容量的驱逐需要指定缓存容量的最大值，当缓存容量达到最大时，Caffeine将使用LRU策略对缓存进行淘汰；基于时间的驱逐策略如字面意思，可以设置在最后访问/写入一个缓存经过指定时间后，自动进行淘汰。
+ *
+ * 驱逐策略可以组合使用，任意驱逐策略生效后，该缓存条目即被驱逐。
+ *
+ * LRU 最近最少使用，淘汰最长时间没有被使用的页面。
+ * LFU 最不经常使用，淘汰一段时间内使用次数最少的页面
+ * FIFO 先进先出
+ * Caffeine有4种缓存淘汰设置
+ *
+ * 大小 （LFU算法进行淘汰）
+ * 权重 （大小与权重 只能二选一）
+ * 时间
+ * 引用 （不常用，本文不介绍）
+ * @author whty
+ * @version 1.0
+ */
+public class CaffeineExpireTest {
+	private static final Logger logger = LoggerFactory.getLogger(CaffeineExpireTest.class);
+
+	/**
+	 * 缓存大小淘汰
+	 */
+	@Test
+	public void maximumSizeTest() throws InterruptedException {
+		Cache<Integer, Integer> cache = Caffeine.newBuilder()
+				//超过10个后会使用W-TinyLFU算法进行淘汰
+				.maximumSize(10)
+				.evictionListener((key, val, removalCause) -> {
+					logger.info("淘汰缓存：key:{} val:{}", key, val);
+				})
+				.build();
+
+		for (int i = 1; i < 20; i++) {
+			cache.put(i, i);
+		}
+		Thread.sleep(500);//缓存淘汰是异步的
+
+		// 打印还没被淘汰的缓存
+		System.out.println(cache.asMap());
+	}
+
+	/**
+	 * 权重淘汰
+	 */
+	@Test
+	public void maximumWeightTest() throws InterruptedException {
+		Cache<Integer, Integer> cache = Caffeine.newBuilder()
+				//限制总权重，若所有缓存的权重加起来>总权重就会淘汰权重小的缓存
+				.maximumWeight(100)
+				.weigher((Weigher<Integer, Integer>) (key, value) -> key)
+				.evictionListener((key, val, removalCause) -> {
+					logger.info("淘汰缓存：key:{} val:{}", key, val);
+				})
+				.build();
+
+		//总权重其实是=所有缓存的权重加起来
+		int maximumWeight = 0;
+		for (int i = 1; i < 20; i++) {
+			cache.put(i, i);
+			maximumWeight += i;
+		}
+		System.out.println("总权重=" + maximumWeight);
+		Thread.sleep(500);//缓存淘汰是异步的
+
+		// 打印还没被淘汰的缓存
+		System.out.println(cache.asMap());
+	}
+
+
+	/**
+	 * 访问后到期（每次访问都会重置时间，也就是说如果一直被访问就不会被淘汰）
+	 */
+	@Test
+	public void expireAfterAccessTest() throws InterruptedException {
+		Cache<Integer, Integer> cache = Caffeine.newBuilder()
+				.expireAfterAccess(1, TimeUnit.SECONDS)
+				//可以指定调度程序来及时删除过期缓存项，而不是等待Caffeine触发定期维护
+				//若不设置scheduler，则缓存会在下一次调用get的时候才会被动删除
+				.scheduler(Scheduler.systemScheduler())
+				.evictionListener((key, val, removalCause) -> {
+					logger.info("淘汰缓存：key:{} val:{}", key, val);
+
+				})
+				.build();
+		cache.put(1, 2);
+		System.out.println(cache.getIfPresent(1));
+		Thread.sleep(3000);
+		System.out.println(cache.getIfPresent(1));//null
+	}
+
+	/**
+	 * 写入后到期
+	 */
+	@Test
+	public void expireAfterWriteTest() throws InterruptedException {
+		Cache<Integer, Integer> cache = Caffeine.newBuilder()
+				.expireAfterWrite(1, TimeUnit.SECONDS)
+				//可以指定调度程序来及时删除过期缓存项，而不是等待Caffeine触发定期维护
+				//若不设置scheduler，则缓存会在下一次调用get的时候才会被动删除
+				.scheduler(Scheduler.systemScheduler())
+				.evictionListener((key, val, removalCause) -> {
+					logger.info("淘汰缓存：key:{} val:{}", key, val);
+				})
+				.build();
+		cache.put(1, 2);
+		Thread.sleep(3000);
+		System.out.println(cache.getIfPresent(1));//null
+	}
+}
