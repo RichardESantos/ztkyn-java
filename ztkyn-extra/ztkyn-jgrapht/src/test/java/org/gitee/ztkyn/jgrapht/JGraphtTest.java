cc0b77880a028c64b5820b07aca475d10b474928
ZtKyn <ZtKyn@lazier.work>
2023-02-14 19:54:11 +0800

    2023-02-14

---
+++ ztkyn-extra/ztkyn-jgrapht/src/test/java/org/gitee/ztkyn/jgrapht/JGraphtTest.java
@@ -0,0 +1,23 @@
+package org.gitee.ztkyn.jgrapht;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author whty
+ * @version 1.0
+ * @description
+ * @date 2023/2/14 18:54
+ */
+public class JGraphtTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(JGraphtTest.class);
+
+	@Test
+	public void testPath() {
+		org.jgrapht.Graph<String, RelationshipEdge> undirectedGraph = new DefaultUndirectedGraph<>(
+				RelationshipEdge.class);
+	}
+
+}
