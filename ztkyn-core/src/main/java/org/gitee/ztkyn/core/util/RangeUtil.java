4f43acb2e47450717b9462d66435dab8bc74ca63
ZtKyn <ZtKyn@lazier.work>
2024-01-15 23:31:07 +0800

    4/01-15-周一

--- ztkyn-core/src/main/java/org/gitee/ztkyn/core/util/RangeUtil.java
+++ ztkyn-core/src/main/java/org/gitee/ztkyn/core/util/RangeUtil.java
@@ -11,27 +11,27 @@ import java.util.List;
  */
 public class RangeUtil {
 
-    private static final Logger logger = LoggerFactory.getLogger(RangeUtil.class);
+	private static final Logger logger = LoggerFactory.getLogger(RangeUtil.class);
 
-    public static int[] rangeInt(int from, int to) {
-        if (to <= from) {
-            logger.error("to is less than from, which is {} < {}", from, to);
-            throw new RuntimeException("to is less than from");
-        }
-        return rangeInt(from, to, 1);
-    }
+	public static int[] rangeInt(int from, int to) {
+		if (to <= from) {
+			logger.error("to is less than from, which is {} < {}", from, to);
+			throw new RuntimeException("to is less than from");
+		}
+		return rangeInt(from, to, 1);
+	}
 
-    public static int[] rangeInt(int from, int to, int step) {
-        List<Integer> integerList = new ArrayList<>();
-        for (int i = from; i < to; i = i + step) {
-            integerList.add(i);
-        }
-        int size = integerList.size();
-        int[] result = new int[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = integerList.get(i);
-        }
-        return result;
-    }
+	public static int[] rangeInt(int from, int to, int step) {
+		List<Integer> integerList = new ArrayList<>();
+		for (int i = from; i < to; i = i + step) {
+			integerList.add(i);
+		}
+		int size = integerList.size();
+		int[] result = new int[size];
+		for (int i = 0; i < size; i++) {
+			result[i] = integerList.get(i);
+		}
+		return result;
+	}
 
 }
