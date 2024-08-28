1fda8f2e88471447eb1f9aa074fd9ef7dd1be595
ZtKyn <ZtKyn@lazier.work>
2024-08-28 11:43:11 +0800

    2024-08-28

--- push.bat
+++ push.bat
@@ -7,6 +7,10 @@ git config user.name "ZtKyn"
 git config user.email "ZtKyn@lazier.work"
 rem 设置中文文件名正常显示
 git config core.quotepath false 
+@rem 开启长名称支持
+git config --system core.longpaths true
+@rem 设置合并（merge）
+git config pull.merge true
 git add * 
 git commit -m %date:~3,4%-%date:~8,2%-%date:~11,2%
 git pull
